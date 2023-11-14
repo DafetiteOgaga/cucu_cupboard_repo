966e9781ab4c6ef3e47c4d182f119329003fd49c
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2023-11-14 21:30:36 +0100

    scrips updated

--- custom_commands_win/runserver_pull.bat
+++ custom_commands_win/runserver_pull.bat
@@ -5,7 +5,7 @@ cd C:\Users\%username%\cucu_cupboard_repo
 
 git pull 2>nul
 if %errorlevel% == 0 (
-  echo Git pull successful.
+  echo update successful.
 ) else (
   echo Git pull failed with error code %errorlevel%. Please check your remote repository connection or maybe you don't have the admin access to this project repository.
 )
