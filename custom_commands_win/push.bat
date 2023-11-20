8d3ca5917f5e96900617f877ed673c0e8f2cb740
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2023-11-20 14:54:04 +0100

    updates

--- custom_commands_win/push.bat
+++ custom_commands_win/push.bat
@@ -103,4 +103,4 @@ REM Push the changes to the remote repository
 git push
 rem ...................................................................
 
-C:\Users\%username%\cucu_cupboard_repo\custom_commands_win C:\Users\%username%\xbin_windows /e /y /d
+xcopy C:\Users\%username%\cucu_cupboard_repo\custom_commands_win C:\Users\%username%\xbin_windows /e /y /d
